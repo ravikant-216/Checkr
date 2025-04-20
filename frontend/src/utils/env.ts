ff58d7b0ea4b0dd5d10fd957d5af57e1687457e1
Ravi Kant <taleshkant@gmail.com>
2025-04-20 16:04:59 +0530

    Fix: url issue

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://react-project-jsonserver-vmp3bm-10e11b-157-180-39-220.traefik.me'
+export const BASE_URL =
+  'http://react-project-jsonserver-vmp3bm-10e11b-157-180-39-220.traefik.me'
