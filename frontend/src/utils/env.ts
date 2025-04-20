ed7d2a33a0ae9f349482a0afa55dbe0e74042d42
Ravi Kant <taleshkant@gmail.com>
2025-04-20 16:29:05 +0530

    Revert "Fix: url issue"
    This reverts commit ff58d7b0ea4b0dd5d10fd957d5af57e1687457e1.

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -1,2 +1,1 @@
-export const BASE_URL =
-  'http://react-project-jsonserver-vmp3bm-10e11b-157-180-39-220.traefik.me'
+export const BASE_URL = 'http://react-project-jsonserver-vmp3bm-10e11b-157-180-39-220.traefik.me'
